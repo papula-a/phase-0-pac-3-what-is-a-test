115e3cdd19a57e84e3663918b1032bb2ddd0a0d9
papula-a <pa20381@auis.edu.krd>
2024-04-10 22:34:39 +0300

    Assignment completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -24,8 +24,8 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
 
+*/
 // The code below ensures that students who are using CodeGrade will get credit 
 // for the code-along in Canvas; you can disregard it.
 
